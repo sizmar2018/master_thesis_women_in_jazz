34b6ea27a64211f44855614c602da219243392f5
sizmar2018 <soumaya_izmar@hotmail.com>
2023-11-11 15:21:11 +0100

    clean data +   get artists info

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -3,6 +3,8 @@ import configparser
 import numpy as np
 import pandas as pd
 import re 
+from bs4 import BeautifulSoup
+import csv
 
 class Utils:
     
@@ -10,13 +12,66 @@ class Utils:
         if config_path != None :
             self.config = configparser.ConfigParser()
             self.config.read(config_path)
+    
+    def get_instruments() :
+        
+        url = "../../data/instruments.html"
+        page = open(url)
+        soup = BeautifulSoup(page.read())
+        ul = soup.find("ul")
+        with open('../../data/instruments.csv', 'w', encoding='Windows-1252') as f:
+            for li in ul.findChildren('li',recursive=False) :
+                main_inst = li.find('a').text
+                sub_ul = li.find('ul')  
+                sub_instruments = ''
+                if sub_ul != None :
+                    for sub_li in sub_ul.findChildren('li',recursive=False) : 
+                        sub_inst = sub_li.find('a').text
+                        sub_inst = sub_inst.replace("\n","")
+                        sub_instruments += sub_inst +  ","
+                sub_instruments = sub_instruments + main_inst
+                csv_line = (main_inst,sub_instruments)
+                w = csv.writer(f)
+                w.writerow(csv_line)
 
-    def clean_role(self,role) : 
-        role = role.lower()
-        role = re.sub("by", "", role)
-        role = re.sub("-", " ", role)
-        role = re.sub(r"\[([A-Za-z0-9_\s\S,]+)\]","", role)
-        return role 
+
+    def cluster_role(self,roles) : 
+        instruments = pd.read_csv('../../data/instruments.csv',sep=',')
+        instruments['sub_instru'] = instruments['sub_instru'].apply(lambda x :x.split(','))
+        clean_roles = list()
+        for r in  roles :          
+            r_strip = r.strip()
+            is_found = False
+            for i in range(0,len(instruments)) :
+                for inst in instruments['sub_instru'][i] :
+                    if  inst.lower() in  r_strip: 
+                        r_strip = instruments['instru'][i].lower()
+                        is_found = True
+                if is_found :
+                    break        
+            clean_roles.append(r_strip)
+        clean_roles  = list(set(clean_roles))
+        return clean_roles    
+
+    def clean_role(self,roles) :
+       
+        roles = roles.lower()
+        roles = re.sub(r"\b[0-9]+\b\s*", "", roles)#remove numbers
+        roles = re.sub("by", "", roles)
+        roles = re.sub("-", " ", roles)
+        roles = re.sub(r"\[([A-Za-z0-9_\s\S,]+)\]","", roles)#remove full []
+        roles = re.sub(r"\[([\s]*)\]","", roles)#remove empty []
+        roles  = roles.split(',')
+
+        clean_roles = self.cluster_role(roles)
+       
+        return clean_roles 
+
+    def clean_artist_name(self,name) :
+        clean_name = name.lower()
+        clean_name = re.sub(r"\(([0-9]+)\)", "", clean_name)
+        clean_name = clean_name.strip()
+        return clean_name
 
     def get_albums_info_from_json(self,tuple):
        
@@ -35,14 +90,9 @@ class Utils:
         artists = list()
         
         for art in extra_artists : 
-            clean_roles = list()
-           
-            role = self.clean_role(art["role"])   
-            roles = role.split(',')
-            for r in  roles :
-                if "a&r" not in roles :
-                    clean_roles.append(r.strip())
-            artists.append({"id":art["id"],"name":art["name"],"role": clean_roles})
+            clean_roles = self.clean_role(art["role"]) 
+            clean_name =  self.clean_artist_name(art["name"])
+            artists.append({"id":art["id"],"name":clean_name,"role": clean_roles})
         #label
         label = tuple['labels'][0]['name']
         return (albums_id,album_title,artists,label,album_style)  
